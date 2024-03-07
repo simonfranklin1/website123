b40e732ea2e8b438aed25a4c28144f237725b9ae
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-03-07 10:01:42 -0300

    feat: Fazendo componente do menu mobile

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,41 +1,52 @@
+import { useState } from "react"
 import "./Navbar.css"
 
-const Navbar = () => {
-  return (
-    <header>
-        <nav className="nav-tela-grande">
-            <a href="#section1">INÍCIO</a>
-            <a href="#section2">OFERECEMOS</a>
-            <a href="#section3">RELATÓRIOS</a>
-            <a href="#section4">WHATSAPP'S</a>
-            <a href="#section5">AUTOMAÇÕES</a>
-            <a href="#section6">API</a>
-            <a href="#section7">SUPORTE ONLINE</a>
-            <a href="#section8">PLANOS</a>
-            <a href="#section9">CONTATO</a>
-        </nav>
+const MobileNavbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    return (
         <nav className="nav-tela-pequena">
-            <div className="navbar">
-                <div className="dropdown">
-                    <button className="dropbtn">
-                        PÁGINAS <i className="fa fa-caret-down"></i>
-                    </button>
-                    <div className="dropdown-content">
-                        <a href="#section1">Início</a>
-                        <a href="#section2">Sobre</a>
-                        <a href="#section3">Recursos</a>
-                        <a href="#section4">Integração</a>
-                        <a href="#section5">Automações</a>
-                        <a href="#section6">API</a>
-                        <a href="#section7">Suporte</a>
-                        <a href="#section8">Planos</a>
-                        <a href="#section9">Contato</a>
-                    </div>
-                </div>
+            <button className="dropbtn" onClick={() => setOpenMenu(prev => !prev)}>
+                {
+                    openMenu ? (
+                     <i class="fa-solid fa-xmark"></i>   
+                    ) : (
+                      <i class="fa-solid fa-bars"></i>  
+                    )
+                }
+            </button>
+            <div className="mobile-menu" style={ openMenu ? { display: "flex"} : { display: "none"} } onClick={() => setOpenMenu(false)}>
+                <a href="#section1">Início</a>
+                <a href="#section2">Sobre</a>
+                <a href="#section3">Recursos</a>
+                <a href="#section4">Integração</a>
+                <a href="#section5">Automações</a>
+                <a href="#section6">API</a>
+                <a href="#section7">Suporte</a>
+                <a href="#section8">Planos</a>
+                <a href="#section9">Contato</a>
             </div>
         </nav>
-    </header>
-  )
+    )
+}
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="nav-tela-grande">
+                <a href="#section1">INÍCIO</a>
+                <a href="#section2">OFERECEMOS</a>
+                <a href="#section3">RELATÓRIOS</a>
+                <a href="#section4">WHATSAPP'S</a>
+                <a href="#section5">AUTOMAÇÕES</a>
+                <a href="#section6">API</a>
+                <a href="#section7">SUPORTE ONLINE</a>
+                <a href="#section8">PLANOS</a>
+                <a href="#section9">CONTATO</a>
+            </nav>
+            <MobileNavbar />
+        </header>
+    )
 }
 
 export default Navbar
